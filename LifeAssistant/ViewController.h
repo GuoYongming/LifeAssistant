dcd73f1e106aebaf42b4318c46cedbc0317ed07c
郭永明 <403501792@qq.com>
2016-01-22 14:42:17 +0800

    Initial Commit

---
+++ LifeAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LifeAssistant
+//
+//  Created by GuoYongming on 1/22/16.
+//  Copyright © 2016 GuoYongming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
